70aa8677edef7b676b828bd875c454feb89c4918
Jeremy Mendoza <jeremy.mendoza@decathlon.com>
2020-02-03 15:42:21 +0100

    dev: modify dev, seeds and seedsDev scripts in package.json

--- config/mailer.config.js
+++ config/mailer.config.js
@@ -1,6 +1,6 @@
 const nodemailer = require('nodemailer');
 
-const APP_HOST = (process.env.NODE_ENV === 'dev') ? 'http://localhost:3000' : process.env.APP_HOST
+const APP_HOST = (process.env.NODE_ENV === 'devMailer') ? 'http://localhost:3000' : process.env.APP_HOST
 
 const user = process.env.MAIL_USER
 const pass = process.env.MAIL_PASS
